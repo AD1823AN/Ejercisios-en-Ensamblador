a7fea34b8c832b4bec017dd913d2c0301d3917e5
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2025-01-31 21:46:46 -0600

    Update sum_1_num.asm

--- sum_1_num.asm
+++ sum_1_num.asm
@@ -1,23 +1,24 @@
+; Autor:
 org 100h     ; Punto de entrada del programa
 
-mov ah, 01    ; Función 01h de DOS: Leer un carácter desde la entrada estándar (teclado)
-int 21h       ; Llama a la interrupción 21h para leer el primer número
-sub al, 30h   ; Convierte el carácter ASCII en un número entero (resta 30h)
-mov bl, al    ; Almacena el primer número en el registro BL
+mov ah, 01    ; FunciÃ³n 01h de DOS: Leer un carÃ¡cter desde la entrada estÃ¡ndar (teclado)
+int 21h       ; Llama a la interrupciÃ³n 21h para leer el primer nÃºmero
+sub al, 30h   ; Convierte el carÃ¡cter ASCII en un nÃºmero entero (resta 30h)
+mov bl, al    ; Almacena el primer nÃºmero en el registro BL
 
-mov ah, 01    ; Función 01h de DOS: Leer un carácter desde la entrada estándar (teclado)
-int 21h       ; Llama a la interrupción 21h para leer el segundo número
-sub al, 30h   ; Convierte el carácter ASCII en un número entero (resta 30h)
-add bl, al    ; Suma el segundo número al primero y almacénalo en BL
+mov ah, 01    ; FunciÃ³n 01h de DOS: Leer un carÃ¡cter desde la entrada estÃ¡ndar (teclado)
+int 21h       ; Llama a la interrupciÃ³n 21h para leer el segundo nÃºmero
+sub al, 30h   ; Convierte el carÃ¡cter ASCII en un nÃºmero entero (resta 30h)
+add bl, al    ; Suma el segundo nÃºmero al primero y almacÃ©nalo en BL
 
-add bl, 30h   ; Convierte el resultado en un carácter ASCII
+add bl, 30h   ; Convierte el resultado en un carÃ¡cter ASCII
 mov dl, bl    ; Coloca el resultado en el registro DL
 
-mov ah, 02    ; Función 02h de DOS: Escribir un carácter en la salida estándar (pantalla)
-int 21h       ; Llama a la interrupción 21h para mostrar el resultado
+mov ah, 02    ; FunciÃ³n 02h de DOS: Escribir un carÃ¡cter en la salida estÃ¡ndar (pantalla)
+int 21h       ; Llama a la interrupciÃ³n 21h para mostrar el resultado
 
-mov ah, 4Ch   ; Función 4Ch de DOS: Terminar el programa
-int 21h       ; Llama a la interrupción 21h para finalizar el programa
+mov ah, 4Ch   ; FunciÃ³n 4Ch de DOS: Terminar el programa
+int 21h       ; Llama a la interrupciÃ³n 21h para finalizar el programa
 
 ret           ; Retorno del programa
 
