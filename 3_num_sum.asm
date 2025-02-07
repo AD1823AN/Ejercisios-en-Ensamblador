d7c8e9f120d1403e83771629cbc6960c99fbf6a7
Adri Trejo <91568721+AD1823AN@users.noreply.github.com>
2025-02-06 23:07:53 -0600

    Update 3_num_sum.asm

--- 3_num_sum.asm
+++ 3_num_sum.asm
@@ -9,7 +9,6 @@ d db 0
 n db 0
 t db 0
 max db 0  ; Variable para almacenar el número mayor
-
 msg db 10,13,"!! Ingrese un numero de 3 digitos !!$"
 msg1 db 10,13,"!! Numero ingresado: !!$"
 msg2 db 10,13,"!! La suma de los numeros es: !!$"
